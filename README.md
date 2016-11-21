153acf987234e103902a5a11b1ac1573879d9e2c
SERKAN AKIN <serkan.akin@netcad.com>
2016-11-21 16:56:51 +0300

    v0.4

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # geoConstraits
-
+version : 0.4
 ##Usage
 
     SELECT geoConstraits( 'tableName',srid,'{"geometryType"}');
---
+++ geoConstraits support sql.sql
@@ -0,0 +1,12 @@
+﻿
+  SELECT 'SELECT geoConstraits('''||table_name||''',32768,''{"GEOMETRY"}'');' from information_schema.columns
+    WHERE table_schema='public' and column_name='poly'
+          AND columns.udt_name='geometry' and is_updatable ='YES'
+    ORDER BY table_name;
+
+
+  SELECT DISTINCT st_srid(poly),st_ndims(poly), geometrytype(poly)
+    from parcel;
+
+  SELECT geoConstraits( 'geopoi',32768,'{"POINT"}');
+  SELECT geoConstraits( 'parcel,32768,'{"POLYGON","MULTIPOLYGON"}');
