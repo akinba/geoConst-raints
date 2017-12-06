8c9bb7d95ca7d11787da71c6c600bf5cfccd7e7b
SERKAN AKIN <serkan.akin@netcad.com>
2017-12-06 17:21:47 +0300

    improvement

--- geoConst.sql
+++ geoConst.sql
@@ -14,9 +14,9 @@
   LANGUAGE plpgsql;
 
 
-  --DROP FUNCTION public.geoconstraits(TEXT,INTEGER,TEXT[] );
+  --DROP FUNCTION public.geoConst(TEXT,INTEGER,TEXT[] );
 
-  CREATE OR REPLACE FUNCTION public.geoConstraits(text, integer, text[])
+  CREATE OR REPLACE FUNCTION public.geoConst(text, integer, text[])
     RETURNS text AS
   $BODY$
   DECLARE r RECORD;
@@ -25,15 +25,15 @@
     --check inputs
     CASE
   WHEN $1 not  in (select tablename::text from pg_tables where schemaname=current_schema())
-    then return 'Hata= Girdiginiz tablo ismi:'||$1||' veri tabanında yok!'; 
-  WHEN 'poly' != (select column_name FROM information_schema.columns WHERE table_schema =  current_schema() AND table_name = 'ada' AND data_type = 'USER-DEFINED')
-    then return 'Hata= Girdiğiniz '||$1||' tablosu geometrik değil veya poly kolonu yok';
+    then return 'Error= Table name :'||$1||' not exist!'; 
+  WHEN 'poly' != (select column_name FROM information_schema.columns WHERE table_schema =  current_schema() AND table_name = $1 AND data_type = 'USER-DEFINED')
+    then return 'Error= '||$1||' doesn''t have geometry column!';
   WHEN $2 not in (select srid from spatial_ref_sys)
-    then return 'Hata= Girdiginiz SRID:'||$2||' public.spatial_ref_sys tablosunda bulunmuyor!';
+    then return 'Error= SRID value:'||$2||' not exist in public.spatial_ref_sys!';
   WHEN $2 != (select geosrid($1))
-    then return 'Hata= Girdiğiniz SRID:'||$2||','||$1||' tablosunun SRID:'||(select geosrid($1))||' ile uyuşmuyor!';
+    then return 'Error= Given SRID:'||$2||','||$1||' mismatch with SRID:'||(select geosrid($1))||' !';
   WHEN array_length($3,1)>3
-    then return 'Hata= En fazla 3 adet geometri tipi belirtebilirsiniz!';
+    then return 'Error= Max geometryType count=3 !';
   ELSE
 
     --drop all Constraits for POLY
@@ -43,11 +43,10 @@
       EXECUTE 'ALTER TABLE IF EXISTS '||$1||' DROP CONSTRAINT IF EXISTS "'||r.constraint_name||'" ;';
     END LOOP;
 
-    --unique
-    EXECUTE 'ALTER TABLE IF EXISTS '||$1||' DROP CONSTRAINT IF EXISTS '||$1||'_objectid_uniq;';
-    EXECUTE 'ALTER TABLE IF EXISTS '||$1||' ADD CONSTRAINT '||$1||'_objectid_uniq UNIQUE(objectid);';
-   --bbox
-    EXECUTE 'ALTER TABLE IF EXISTS '||$1||' ADD CONSTRAINT enforce_bbox_poly CHECK (st_intersects(st_pointonsurface(poly),st_makeenvelope(0::double precision, 4000000::double precision,  600000::double precision,  5000000::double precision, '||$2||')) = true);';
+    --isvalid
+    EXECUTE 'ALTER TABLE IF EXISTS '||$1||' ADD CONSTRAINT enforce_isvalid_poly CHECK (st_isvalid(poly) = true);';
+    --bbox
+    EXECUTE 'ALTER TABLE IF EXISTS '||$1||' ADD CONSTRAINT enforce_bbox_poly CHECK (st_intersects(st_pointonsurface(poly),st_makeenvelope(0::double precision, 4000000::double precision,  700000::double precision,  5000000::double precision, '||$2||')) = true);';
     --ndims
     EXECUTE 'ALTER TABLE IF EXISTS '||$1||' ADD CONSTRAINT enforce_dims_poly CHECK (st_ndims(poly) = 2);';
     --srid
@@ -60,14 +59,13 @@
       ELSEIF array_length($3,1)=3 THEN
         EXECUTE 'ALTER TABLE IF EXISTS '||$1||' ADD CONSTRAINT enforce_gtype_poly CHECK (geometrytype(poly) in ('''||$3[1]||''','''||$3[2]||''','''||$3[3]||'''));';
         END IF ;
-    --isvalid
-    EXECUTE 'ALTER TABLE IF EXISTS '||$1||' ADD CONSTRAINT enforce_isvalid_poly CHECK (st_isvalid(poly) = true);';
+
     return 'OK';
   end CASE;
   END
 
     $BODY$
     LANGUAGE plpgsql;
-  COMMENT ON FUNCTION public.geoconstraits(text, integer, text[]) IS '[BSA] v0.4';
+  COMMENT ON FUNCTION public.geoConst(text, integer, text[]) IS '[BSA] v0.5';
 
 
--- geoConstraints support.sql
+++ geoConstraints support.sql
@@ -1,5 +1,5 @@
 ﻿
-  SELECT 'SELECT geoConstraits('''||table_name||''',32768,''{"GEOMETRY"}'');' from information_schema.columns
+  SELECT 'SELECT geoConst('''||table_name||''',32768,''{"GEOMETRY"}'');' from information_schema.columns
     WHERE table_schema='public' and column_name='poly'
           AND columns.udt_name='geometry' and is_updatable ='YES'
     ORDER BY table_name;
@@ -8,5 +8,5 @@
   SELECT DISTINCT st_srid(poly),st_ndims(poly), geometrytype(poly)
     from parcel;
 
-  SELECT geoConstraits( 'geopoi',32768,'{"POINT"}');
-  SELECT geoConstraits( 'parcel',32768,'{"POLYGON","MULTIPOLYGON"}');
+  SELECT geoConst( 'geopoi',32768,'{"POINT"}');
+  SELECT geoConst( 'parcel',32768,'{"POLYGON","MULTIPOLYGON"}');
