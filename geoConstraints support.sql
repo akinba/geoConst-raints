8afd1a8cf49060829f46dbaeeaac20fa9360db0f
SERKAN AKIN <akinba@gmail.com>
2019-01-17 13:16:40 +0300

    'update'

--- geoConstraints support.sql
+++ geoConstraints support.sql
@@ -1,12 +1,12 @@
 ﻿
-  SELECT 'SELECT geoConst('''||table_name||''',32768,array['POLYGON']);' from information_schema.columns
+  SELECT 'SELECT geoConst('''||table_name||''',32768,array[''POLYGON'']);' from information_schema.columns
     WHERE table_schema='public' and column_name='poly'
           AND columns.udt_name='geometry' and is_updatable ='YES'
     ORDER BY table_name;
 
 
   SELECT DISTINCT st_srid(poly),st_ndims(poly), geometrytype(poly)
-    from parcel;
+    from tableOne;
 
 	SELECT geoConst( 'poi', 4326, array['POINT']);
   	SELECT geoConst( 'area',2320, array['POLYGON','MULTIPOLYGON']);
